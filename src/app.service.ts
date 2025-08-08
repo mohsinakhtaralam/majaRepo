931c40045f8f9c13280e6f25230ca2f1818c7d8f
Mohsin Akhtar Alam <mdxb.info@gmail.com>
2025-08-08 16:46:12 +0500

    second commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Maja! Teri meharbani chal jaein ';
+    return 'Hello Maja! Teri meharbani chal jaein yara ';
   }
 }
