371778fda45c656a9d08d573af7ea3fcc5ba5d6a
Mohsin Akhtar Alam <mdxb.info@gmail.com>
2025-08-08 16:48:09 +0500

    third one , chal

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Maja! Teri meharbani chal jaein yara ';
+    return 'Hello Maja! Welcome to the NestJS application.';
   }
 }
